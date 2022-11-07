dbf8b9aef90e83f40f2d183be26c486cadffb59f
Jose Peinado <jd.peinado@platzi.com>
2022-11-07 10:56:20 -0600

    upate config js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_SuBpsv6st', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1oq4t27tvkc3d5rg94m7ccqs7k', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
